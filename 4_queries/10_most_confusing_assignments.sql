03348f170962680c1fc314256f73a34404a665ee
Iyris Vigil <iyrisvigil@gmail.com>
2020-09-16 04:35:37 +0000

    add last round of queries

---
+++ 4_queries/10_most_confusing_assignments.sql
@@ -0,0 +1,8 @@
+-- List each assignment with the total number of assistance requests with it.
+-- Order by total assistances in order of most to least.
+
+SELECT assignments.id, assignments.name, day, chapter, count(assistance_requests) as total_requests
+FROM assignments
+JOIN assistance_requests ON assignment_id = assignments.id
+GROUP BY assignments.id
+ORDER BY total_requests DESC;
---
+++ 4_queries/8_total_cohort_assistance_duration.sql
@@ -0,0 +1,10 @@
+-- Get the total duration of all assistance requests for each cohort.
+-- Order by total_duration.
+
+
+SELECT cohorts.name as cohort, SUM(completed_at-started_at) as total_duration
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+JOIN assistance_requests ON student_id = students.id
+GROUP BY cohorts.name
+ORDER BY total_duration;
